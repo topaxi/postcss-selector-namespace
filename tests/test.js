c2ce73860219502ffda466ca19e52fbea87ae0ec
Damian Senn <damian.senn@gmail.com>
2016-01-06 14:45:42 +0100

    Fix using regexp with multiple matches.
    Executing the same regexp advances their index.

--- tests/test.js
+++ tests/test.js
@@ -20,4 +20,15 @@ describe('Basic functionality', () => {
 
     expect(String(css)).to.equal(String(expected))
   })
+
+  it('works with a regexp which matches multiple selectors', () => {
+    let { css } = transform(
+      fs.readFileSync(`${__dirname}/fixtures/multiself.css`),
+      { selfSelector: /&|:--component/, namespace: '.my-component' }
+    )
+
+    let expected = fs.readFileSync(`${__dirname}/expected/multiself.css`)
+
+    expect(String(css)).to.equal(String(expected))
+  })
 })
